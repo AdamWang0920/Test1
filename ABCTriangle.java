420658597859a5afe841adf9eb00687e94ab0094
AdamWang <pos.wang0920@gmail.com>
2020-11-08 16:34:45 +0800

    ABC triangle

---
+++ ABCTriangle.java
@@ -0,0 +1,32 @@
+
+public class ABCTriangle {
+	public static void main(String[] args) {
+		for(int i=1;i<7;i++){
+		    for(int j=1;j<=i;j++){
+		    	switch(i) {
+		    	case 1:
+		    		System.out.print("A"+" ");
+		    		break;
+		    	case 2:
+		    		System.out.print("B"+" ");
+		    		break;
+		    	case 3:
+		    		System.out.print("C"+" ");
+		    		break;
+		    	case 4:
+		    		System.out.print("D"+" ");
+		    		break;
+		    	case 5:
+		    		System.out.print("E"+" ");
+		    		break;
+		    	case 6:
+		    		System.out.print("F"+" ");
+		    		break;	
+		    	}
+		        
+		     }       
+		    System.out.println();
+		}
+	}
+
+}
